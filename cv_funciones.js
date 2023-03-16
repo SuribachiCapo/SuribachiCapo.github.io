88d995127cd031cbc2512598166fab960a603f5b
SuribachiCapo <127221608+SuribachiCapo@users.noreply.github.com>
2023-03-16 13:05:16 -0300

    Add files via upload

---
+++ cv_funciones.js
@@ -0,0 +1,22 @@
+//DEFINICION DE FUNCIONES//
+
+var ref1= "Ernest pasó largos años con nosotros demostrando ser un gran profesional y excelente persona. Todavía se recuerdan en Montreal su cortes especiales para Huskys! (Peter Suskind, CEO)";
+var ref2= "Si alguien sabía manejar la tijera con estilo, ese fue Ernest Arrakis. Su impronta se preserva aun en nuestra institución. Un gran profesional! (Walter Concryte, Owner)";
+var ref3= "Ernest entrego toda su potencial estilistico a nuestros fieles dogos argentinos. Gracias Ernest! (Mayor Hugo Pedernera, Jefe División Perros PFA)"
+
+function MuestraReferencia1 () {
+    document.getElementById("demo").innerHTML = ref1
+    }
+
+function MuestraReferencia2 () {
+    document.getElementById("demo").innerHTML = ref2
+    }
+
+function MuestraReferencia3 () {
+    document.getElementById("demo").innerHTML = ref3
+    }
+
+
+
+
+/*"";*/
